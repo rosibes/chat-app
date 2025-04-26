aa76036e6d64a50a2b1685a11da80ac765443d60
rosibes <seby.boiciuc03@e-uvt.ro>
2025-04-26 16:59:52 +0300

    added signin endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt";
@@ -13,39 +13,50 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.post("/signup", async (req, res) => {
+app.post("/signup", async (req: Request, res: Response) => {
     try {
-        const parsedData = CreateUserSchema.safeParse(req.body)
+        const parsedData = await CreateUserSchema.safeParse(req.body)
         if (!parsedData.success) {
-            res.json({
-                message: "Invalid inputs"
+            res.status(400).json({
+                message: "Invalid inputs",
+                errors: parsedData.error.flatten(),
             })
             return
         }
 
-        const user = await prismaClient.user.findFirst({
+        const { username, password, name } = parsedData.data;
+
+
+        const existingUser = await prismaClient.user.findFirst({
             where: {
-                email: parsedData.data?.username
+                email: username
             }
         });
 
-        if (user) {
+        if (existingUser) {
             res.status(411).json({
                 message: "User aleady exists"
             })
             return
         }
 
-        const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
-
-        prismaClient.user.create({
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await prismaClient.user.create({
             data: {
-                email: parsedData.data.username,
-                name: parsedData.data.name,
+                email: username,
+                name: name,
                 password: hashedPassword
             }
         })
+
+        res.status(201).json({
+            userId: user.id,
+            message: "User created successfully"
+        });
+        return
+
     } catch (err) {
+        console.error('Error during signup:', err);
         res.status(500).json({
             message: "Internal server error"
         });
@@ -54,24 +65,58 @@ app.post("/signup", async (req, res) => {
 
 })
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req: Request, res: Response) => {
+    try {
+        const parsedData = SignInSchema.safeParse(req.body)
+        if (!parsedData.success) {
+            res.status(400).json({
+                message: "Invalid inputs",
+                errors: parsedData.error.flatten(),
+            })
+            return
+        }
 
-    const data = SignInSchema.safeParse(req.body)
-    if (!data.success) {
-        res.json({
-            message: "Invalid inputs"
-        })
+        const { username, password } = parsedData.data
+
+        const existingUser = await prismaClient.user.findFirst({
+            where: {
+                email: username,
+            }
+        });
+
+        if (!existingUser) {
+            res.status(401).json({
+                message: "Invalid email or password",
+            });
+            return
+        }
+        const isValidPassword = await bcrypt.compare(password, existingUser.password)
+        if (!isValidPassword) {
+            res.status(401).json({
+                message: "Invalid email or password",
+            });
+            return
+        }
+
+        const userId = existingUser.id;
+        const token = jwt.sign({
+            userId
+        }, JWT_SECRET, { expiresIn: "1h" })
+
+        res.status(200).json({
+            userId: userId,
+            token: token,
+            message: "Sign in succesfull!"
+        });
+        return
+
+    } catch (err) {
+        console.error('Error during signup:', err);
+        res.status(500).json({
+            message: "Internal server error"
+        });
         return
     }
-
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET)
-
-    res.json({
-        token: token
-    })
 })
 
 app.post("/room", authMiddleware, (req, res) => {
